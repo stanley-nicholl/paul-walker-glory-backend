3453de573866d4c8785c95b755cbbfdeb5ef8809
Stanley <snicholl@gmail.com>
2017-11-10 11:40:26 -0800

    fixed knexfile

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   production: {
-    client: 'prostgresql',
+    client: 'postgresql',
     connection: 'process.env.DATABASE_URL',
     migrations: {
       directory: path.join(__dirname, 'db', 'migrations')
